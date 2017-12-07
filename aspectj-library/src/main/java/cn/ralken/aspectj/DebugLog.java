8f80bc962b3aef6c44ff46c3c54e243c2c09f7f5
Ralken Liao <zeroyakon@gmail.com>
2017-12-07 14:46:50 +0800

    Print log on logcat with android.util.Log

--- aspectj-library/src/main/java/cn/ralken/aspectj/DebugLog.java
+++ aspectj-library/src/main/java/cn/ralken/aspectj/DebugLog.java
@@ -1,8 +1,18 @@
 package cn.ralken.aspectj;
 
+import android.util.Log;
+
 /**
  * Created by Ralken Liao on 07/12/2017.
  */
 
 public class DebugLog {
+    private static final String LOG_TAG = "aspectj-log";
+
+    private DebugLog() {
+    }
+
+    public static void log(String message) {
+        Log.d(LOG_TAG, message);
+    }
 }
